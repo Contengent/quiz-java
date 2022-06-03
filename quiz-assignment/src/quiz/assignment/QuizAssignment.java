cbaa2873872ba0af5e6c19485069b27b7f42fe39
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 00:29:00 -0400

    Add files via upload

---
+++ quiz-assignment/src/quiz/assignment/QuizAssignment.java
@@ -0,0 +1,19 @@
+package quiz.assignment;
+/**
+ * @author 
+ *
+ *   /\_/\  _     ___
+ *  |>'v'<||"| -/\, ,`>
+ *     -=Edelstein=-
+ * 
+ */
+public class QuizAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
